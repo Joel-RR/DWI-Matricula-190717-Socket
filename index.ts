cc99710703b70dad8b04a68d3d7c3ced5cdeedc6
Joel Ricaño Rodriguez <joel.ricanorodriguez@utxicotepec.edu.mx>
2022-06-22 14:30:43 -0500

    Segundo commit: Implementacion del patron de diseño singlento en la clase Server

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import router from "./routes/router";
 import bodyParser from "body-parser";
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 
 
 server.app.use(bodyParser.urlencoded ({ extended: true}) );
@@ -14,5 +14,5 @@ server.app.use(cors({origin:true, credentials: true}));
 server.app.use('/', router);
 
 server.start( ()=> {
- console.log(`Servidor corriendo en el puerto ${server.port}`)
+ console.log(`Servidor corriendo en el puerto ${server.port}`);
 });
