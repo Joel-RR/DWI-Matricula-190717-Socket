5ff4b7f50a9d8cc82e8d66d1c64e6886132a5bac
Joel Ricaño <joel.ricanorodriguez@utxicotepec.edu.mx>
2022-07-13 14:37:07 -0500

    Implementaciuon de las clases usuarios y listausuarios con sus metodos: agregar, borrar, actualizar

--- sockets/sockets.ts
+++ sockets/sockets.ts
@@ -1,9 +1,18 @@
 import { Socket } from "socket.io";
 import socketIO from 'socket.io';
+import { UsuariosLista } from "../classes/usuarios-lista";
+import { Usuario } from "../classes/usuario";
+
+export const usuariosConectados = new UsuariosLista();
+export const conectarCliente = (cliente: Socket) =>{
+    const usuario = new Usuario( cliente.id );
+    usuariosConectados.agregar(usuario);
+}
 
 export const desconectar = (cliente: Socket) =>{
     cliente.on('disconnect', ()=>{
-        console.log('Cliente Desconectado')
+        console.log('Cliente Desconectado');
+        usuariosConectados.borrarUsuario( cliente.id );
     });
 }
 
@@ -18,8 +27,15 @@ export const mensaje = (cliente: Socket, io: socketIO.Server) =>
 
 export const configurarUsuario = (cliente: Socket, io: socketIO.Server) =>
 {
-    cliente.on('configurar-usuario', (payload: { nombre: string}) => {
+    cliente.on('configurar-usuario', (payload: { nombre: string}, callback: Function) => {
+
+        usuariosConectados.actualizarNombre(cliente.id,payload.nombre);
         console.log('Configurando Usuario', payload);
+
+        callback({
+            ok: true,
+            mensaje: `Usuario ${payload.nombre}, configurado`
+        })
     });
 }
 
